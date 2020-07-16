7086c7e7776999f39fd3ab6d88c7a3a9bd967136
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-07-16 22:47:37 +0200

    feat: build navbar for the counted sections

--- js/app.js
+++ js/app.js
@@ -24,17 +24,36 @@
  * Start Helper Functions
  * 
 */
+const Sections = document.querySelectorAll('section');
+console.log(Sections);
+const navbarUl = document.querySelector('#navbar__list');
+console.log(navbarUl);
 
+const createLi = (sectionNum) => {
+    const li = document.createElement('li');
+    li.classList = ['navbar__menu']
+    const a = document.createElement('a');
+    a.setAttribute('href', `#section${sectionNum}`);
+    a.textContent = `Section${sectionNum}`;
+    a.classList = ['menu__link']
+    li.appendChild(a);
+    
+    return li;
+}
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
-
+for (let i = 0; i<Sections.length; i++){
+    sectionNum = i+1;
+    const li = createLi(sectionNum);
+    navbarUl.appendChild(li);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,7 +64,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
