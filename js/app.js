dbf4dc992f2bfd75f9fd7655be2be43f6b460f9b
Ahmed Elmaghallawy <ahmedelmaghallawy@protonmail.com>
2020-07-17 12:12:28 +0200

    feat: add a section dyamically and rebuild nav

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@
  * Define Global Variables
  * 
 */
-const sections = document.querySelectorAll('section');
+
 const navbarUl = document.querySelector('#navbar__list');
 const toTopbutton = document.getElementById("toTopBtn");
 /**
@@ -25,6 +25,9 @@ const toTopbutton = document.getElementById("toTopBtn");
  * Start Helper Functions
  * 
 */
+
+// add section dynamically
+
 
 
 const createLi = (sectionNum) => {
@@ -47,6 +50,7 @@ const ElementLocation = (ele) => {
 }
 
 const sectionLocations = () => {
+    const sections = document.querySelectorAll('section');
     const SectionTopPoints = [];
     for (sec of sections) {
         SectionTopPoints.push(ElementLocation(sec));
@@ -61,11 +65,6 @@ const sectionLocations = () => {
 */
 
 // build the nav
-for (let i = 0; i < sections.length; i++) {
-    sectionNum = i + 1;
-    const li = createLi(sectionNum);
-    navbarUl.appendChild(li);
-}
 
 // Add class 'active' to section link when near top of viewport
 const scrollToActivate = () => {
@@ -112,10 +111,45 @@ const scrollToTop = () => {
  *
 */
 
-// Build menu 
 
+// add section dynamically 
+addSectionDynamically();
+// build the navbar if the DOM is loaded 
+window.addEventListener("DOMContentLoaded", buildNavBar);
 // Scroll to section on link click
 navbarUl.addEventListener('click', scrollToClickedSection);
-// Set sections as active
+// Set sections links as active while scrolling through section
 window.addEventListener("scroll", scrollToActivate);
+
+function buildNavBar() {
+    const sections = document.querySelectorAll('section');
+    for (let i = 0; i < sections.length; i++) {
+        sectionNum = i + 1;
+        const li = createLi(sectionNum);
+        navbarUl.appendChild(li);
+    }
+}
+
+function addSectionDynamically() {
+    const dynamicallyAddedSec = document.createElement('section');
+    dynamicallyAddedSec.setAttribute('id', 'section4');
+    dynamicallyAddedSec.setAttribute('data-nav', 'Section 4');
+    dynamicallyAddedSec.innerHTML = `
+      <div class="landing__container">
+        <h2>Section 4</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra
+          dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus
+          imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget
+          bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet
+          elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo
+          nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie
+          semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel
+          luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur
+          porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+      </div>
+   `;
+    document.querySelector('main').appendChild(dynamicallyAddedSec);
+}
 
